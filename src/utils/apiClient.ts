7ff2e6662daf3e7372c554d8b0fd7ddebc47b89b
agnesnora <agnesnora@gmail.com>
2026-01-02 14:33:04 +0100

    changing jsonport to 3001

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000", // json mock server
+  baseURL: "http://localhost:3001", // json mock server
   headers: {
     "Content-Type": "application/json",
   },
