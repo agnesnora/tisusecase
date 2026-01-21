93fc2a215cf5ba7310d0e23d0e0a54a5fe633b02
agnesnora <agnesnora@gmail.com>
2026-01-21 15:02:12 +0100

    updating server deployed

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3001", // json mock server
+  // baseURL: "http://localhost:3001", // json mock server
+  baseURL: "https://meter-mock-json.onrender.com", // json mock server
   headers: {
     "Content-Type": "application/json",
   },
