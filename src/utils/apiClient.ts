836dd07d92bef93bf2d4ba17a13178eaab636c2c
agnesnora <agnesnora@gmail.com>
2026-01-26 15:18:06 +0100

    fallback data to loasd faster

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,11 +1,26 @@
 import axios from "axios";
-
+import fallbackData from "../../db 1.json";
 const apiClient = axios.create({
   // baseURL: "http://localhost:3001", // json mock server
   baseURL: "https://meter-mock-json.onrender.com", // json mock server
   headers: {
     "Content-Type": "application/json",
   },
+  timeout: 5000,
 });
+apiClient.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const url = error.config.url;
+    if (url === "/meters") return { data: fallbackData.meters };
+    if (url === "/readings") return { data: fallbackData.readings };
+    if (url.startsWith("/meters/")) {
+      const id = url.split("/")[2];
+      const meter = fallbackData.meters.find((m) => m.id === id);
+      return { data: meter };
+    }
+    throw error;
+  },
+);
 
 export default apiClient;
